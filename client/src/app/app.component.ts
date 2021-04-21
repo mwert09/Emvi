e62a5b65e131cb9d923d64568619a12ee5c56589
mwert09 <mwert09@gmail.com>
2021-04-21 19:07:58 +0300

    Building the UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +7,8 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Emvi';
-  products: any[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-        this.products = response.data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+  ngOnInit(): void {}
 }
