3fac9135648c0d85ca87c62e3534fbfcd37afe8d
mwert09 <mwert09@gmail.com>
2021-05-05 16:17:33 +0300

    Validation API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -88,6 +88,9 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistsAsync(registerDto.Email).Result.Value){
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{"Email address taken."}});
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
